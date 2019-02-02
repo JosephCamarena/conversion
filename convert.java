36b24f433ffb77d0e02b0c8925b85e6f1fbcfab1
Joseph Camarena <joseph.a.camarena@gmail.com>
2019-02-02 15:00:27 -0800

    Update convert java file
    What/Why
    Program should now convert any number base system (excluding binary) to decimal, with hard code.
    
    [Ticket: 2]

--- convert.java
+++ convert.java
@@ -1,6 +1,25 @@
+import java.util.*;
+
 public class convert {
 
 	public static void main (String[] args) {
-	System.out.println("Hello World!");
-    }
+
+		int digit_ctr = 2;
+		int exp = 1;
+		int index = 0;
+		int total_decimal = 0;
+		int base = 8;
+		ArrayList<Integer> input = new ArrayList<Integer>();
+		input.add(1);
+		input.add(1);
+
+		do {
+			total_decimal += input.get(index) * Math.pow(base, exp);
+			digit_ctr--;
+			index++;
+			exp--;
+    }while(digit_ctr > 0);
+
+		System.out.println(total_decimal);
+	}
 }
